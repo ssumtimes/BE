8b4546275ff07f9ea72c855690700e82ef5fe027
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-09-11 12:56:26 +0900

    initial commit

---
+++ src/main/java/org/sometimes/sometimes/SometimesApplication.java
@@ -0,0 +1,13 @@
+package org.sometimes.sometimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SometimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SometimesApplication.class, args);
+    }
+
+}
