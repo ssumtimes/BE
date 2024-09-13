5fe4e827e7240c986dab1b22cb37dcfa4ecf5c48
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-09-13 23:49:21 +0900

    feat: 컨트롤러 및 기능 요구 추가

---
+++ src/main/java/org/sometimes/sometimes/apply/web/controller/ApplyController.java
@@ -0,0 +1,31 @@
+package org.sometimes.sometimes.apply.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/apply")
+@RequiredArgsConstructor
+@Tag(name = "[USER] 회원관련 API")
+public class ApplyController {
+
+    /*
+     * 신청서 작성
+     */
+
+
+    /*
+      신청서 수정
+     */
+
+
+    /*
+     * 작성한 신청서 조회
+     */
+}
+
